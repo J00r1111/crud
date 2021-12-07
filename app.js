3da501634daae3d67449538b774755d929420431
J00r1111 <74411358+J00r1111@users.noreply.github.com>
2021-12-07 13:38:20 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+app.set('view engine', 'hbs');
+app.use(express.urlencoded());
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://marcin:marcin@cluster0.rotxh.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+let db;
+
+client.connect(err => {
+    db = client.db("test").collection("studenci");
+   
+});
+app.get('/', function (req, res) {
+    res.render('formularz');
+})
+app.post('/', function (req, res) {
+    db.insertOne({ imie: req.body.imie, nazwisko: req.body.nazwisko }, function (err, res) {
+        if (err) throw err;
+        
+    });
+    res.render('udalosie');
+   
+})
+
+
+app.listen(5000, () =>{
+    console.log("Uruchomiono aplikacje na porcie 5000");
+    console.log("By Marcin");
+    
+});
+client.close();
